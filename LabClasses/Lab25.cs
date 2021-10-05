9e5853f47ad1e403f05ff35e476b4baefbb29329
Oleg54 <49450466+Oleg54@users.noreply.github.com>
2021-10-05 13:15:31 +0300

    Update Lab25.cs

--- LabClasses/Lab25.cs
+++ LabClasses/Lab25.cs
@@ -1,1 +1,24 @@
+using System;
+using System.IO;
 
+namespace Labs
+{
+    public static class Lab25
+    {
+        //Метод для вывода результатов, вызывается из класса Program
+        public static void WriteAllExercises()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ответы на лабораторную 24");
+
+            Console.Write("Задание 1 (): ");
+        }
+
+
+        public static int GetAnswerToExercise1(string _input)
+        {
+            
+            
+        }
+    }
+}
