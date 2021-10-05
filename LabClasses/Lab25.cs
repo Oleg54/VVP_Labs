972809b8474b62590c03539e9c5b9c7a42b798fc
Oleg54 <49450466+Oleg54@users.noreply.github.com>
2021-10-05 13:35:36 +0300

    Update Lab25.cs

--- LabClasses/Lab25.cs
+++ LabClasses/Lab25.cs
@@ -5,20 +5,26 @@ namespace Labs
 {
     public static class Lab25
     {
+        public static void Main() { WriteAllExercises(); }
+
         //Метод для вывода результатов, вызывается из класса Program
         public static void WriteAllExercises()
         {
             Console.WriteLine();
-            Console.WriteLine("Ответы на лабораторную 25");
+            Console.WriteLine("Ответы на лабораторную 24");
 
-            Console.Write("Задание 1 (): ");
+            Console.Write("Задание 1 (удалить из файла символы, стоящие перед пробелом): ");
+            AnswerToExercise1("C:/Users/o.k.kravchenko/Desktop/text1.txt");
         }
 
 
-        public static int GetAnswerToExercise1(string _input)
+        public static void AnswerToExercise1(string path)
         {
+            string text = File.ReadAllText(path);
+
+            string result = text.Substring(text.IndexOf(' ') + 1);
             
-            
+            File.WriteAllText(path, result);
         }
     }
 }
